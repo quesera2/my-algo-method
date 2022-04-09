0463b0a4a0c5ab8d88ed85972136a3f6091e80de
quesera2 <quesera2262@gmail.com>
2022-04-09 20:47:27 +0900

    for文を使わない書き方に修正

--- data-structure/algo-864.swift
+++ data-structure/algo-864.swift
@@ -1,15 +1,13 @@
 // Q6. 一致する確率
 // https://algo-method.com/tasks/864
 let N = Double(readLine()!)!
+// 重複する数を辞書から得る
 let A = Dictionary(readLine()!.split(separator: " ").map { ($0, 1.0) },
                    uniquingKeysWith: +)
-
-// それぞれの整数について、2つ選んだとき一致する確率の和を出す
-var result = 0.0
-for (_, value) in A {
-    if value > 1 {
-        result += (value / N) * ((value - 1) / (N - 1))
-    }
-}
+// それぞれの数について2つ選んだとき一致する確率を算出して合算する
+let result = A.values
+              .filter { $0 > 1 }
+              .map { ($0 / N) * (($0 - 1) / (N - 1)) }
+              .reduce(0.0, +)
 
 print(result)
