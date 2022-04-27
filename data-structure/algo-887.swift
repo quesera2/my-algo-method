1d22d101b24c445b03ea0141d447984e484d22b9
quesera2 <quesera2262@gmail.com>
2022-04-27 21:22:08 +0900

    左端限定なので助かった…

---
+++ data-structure/algo-887.swift
@@ -0,0 +1,31 @@
+// 括弧列の対応関係
+// https://algo-method.com/tasks/887
+
+let N = Int(readLine()!)!
+let S = readLine()!
+
+struct Stack<T> {
+    var items:[T] = []
+
+    mutating func push(_ item: T) {
+        items.append(item)
+    }
+    mutating func pop() -> T {
+        return items.removeLast()
+    }
+}
+
+var stack = Stack<Int>()
+var result: [Int] = []
+
+for (index, s) in S.enumerated() {    
+    if s == "(" {
+        stack.push(index)
+    } else if s == ")" {
+        let popIndex = stack.pop()
+        if popIndex == 0 {
+            print(index)
+            break
+        }
+    }
+}
